837578d0741cd2047909e1e4cee5c3daeaf5369a
dantleech <dan.t.leech@gmail.com>
2014-06-17 20:07:49 +0200

    Added shell mode for embedded application

--- src/PHPCR/Shell/Console/Application/EmbeddedApplication.php
+++ src/PHPCR/Shell/Console/Application/EmbeddedApplication.php
@@ -2,14 +2,30 @@
 
 namespace PHPCR\Shell\Console\Application;
 
+use PHPCR\Shell\Event\ApplicationInitEvent;
+use PHPCR\Shell\Event\PhpcrShellEvents;
+
 class EmbeddedApplication extends ShellApplication
 {
-    public function __construct($name = 'UNKNOWN', $version = 'UNKNOWN')
+    /**
+     * Do not provide shell features
+     */
+    const MODE_COMMAND = 'command';
+
+    /**
+     * Provide shell features
+     */
+    const MODE_SHELL = 'shell';
+
+    protected $mode;
+
+    public function __construct($name = 'UNKNOWN', $version = 'UNKNOWN', $mode = self::MODE_COMMAND)
     {
         parent::__construct(SessionApplication::APP_NAME, SessionApplication::APP_VERSION);
 
         $this->registerHelpers();
         $this->registerEventListeners();
+        $this->mode = $mode;
     }
 
     public function init()
@@ -19,6 +35,14 @@ class EmbeddedApplication extends ShellApplication
         }
 
         $this->registerPhpcrCommands();
+
+        if ($this->mode == self::MODE_SHELL) {
+            $this->registerShellCommands();
+        }
+
+        $event = new ApplicationInitEvent($this);
+        $this->dispatcher->dispatch(PhpcrShellEvents::APPLICATION_INIT, $event);
+
         $this->initialized = true;
     }
 
